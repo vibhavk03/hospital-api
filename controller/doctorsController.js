6824988688379de229593c99362c1eb9d7e944e7
vibhavk03 <vibhavkashyap3@gmail.com>
2023-07-11 23:11:34 +0530

    adding custom error messages and validations

--- controller/doctorsController.js
+++ controller/doctorsController.js
@@ -1,15 +1,35 @@
 const Doctor = require('../models/Doctor');
 
+const handleErrors = function (err) {
+  let errors = { email: 'no error', password: 'no error' };
+
+  /* handle duplicate email register */
+  if (err.code === 11000) {
+    errors.email = 'This email is already registered';
+    return errors;
+  }
+
+  if (err.message.includes('doctor validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      const { message, path } = properties;
+      errors[path] = message;
+    });
+  }
+
+  return errors;
+};
+
 module.exports = {
   register: async function (req, res) {
     try {
       const doctor = await Doctor.create(req.body);
       res.status(201).send(doctor);
-    } catch (error) {
-      console.log(`error in registering a doctor : ${error}`);
+    } catch (err) {
+      console.log(`error in registering a doctor : ${err}`);
+      const error = handleErrors(err);
       res.status(400).send({
         message: 'error in registering a doctor',
-        error: `${error}`,
+        error,
       });
     }
   },
--- models/Doctor.js
+++ models/Doctor.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const doctorSchema = new mongoose.Schema(
   {
     email: {
       type: String,
       trim: true,
-      required: true,
+      required: [true, 'Please enter an email'],
       unique: true,
       lowercase: true,
+      validate: [isEmail, 'Please enter a valid email'],
     },
     password: {
       type: String,
-      required: true,
-      minLength: 6,
+      required: [true, 'Please enter a password'],
+      minLength: [6, 'Minimum password length is 6 characters'],
     },
   },
   {
