5b52cc11506e6eee4aa335a732a8cd6912fc6d09
vibhavk03 <vibhavkashyap3@gmail.com>
2023-07-16 22:17:49 +0530

    adding create report route

--- controller/patientsController.js
+++ controller/patientsController.js
@@ -1,6 +1,7 @@
 const Patient = require('../models/Patient');
+const Report = require('../models/Report');
 
-const handleErrors = async function (err, body) {
+const handleErrorsForPatient = async function (err, body) {
   let errors = { name: '', age: '', sex: '', phone: '' };
 
   /* patient register error handling */
@@ -23,14 +24,36 @@ const handleErrors = async function (err, body) {
   return errors;
 };
 
+const handleErrorsForReport = function (err) {
+  let errors = { patientId: '', status: '' };
+
+  /* patient register error handling */
+  if (err.message.includes('report validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      const { message, path } = properties;
+      errors[path] = message;
+    });
+  }
+
+  /* if patient id is not valid */
+  if (err.message.includes('Cast to ObjectId failed')) {
+    errors.patientId = 'invalid patient id';
+  }
+
+  return errors;
+};
+
 module.exports = {
   register: async function (req, res) {
     try {
       const patient = await Patient.create(req.body);
-      res.status(201).send(patient);
+      res.status(201).send({
+        message: 'patient registered',
+        patient,
+      });
     } catch (err) {
       console.log(`error in registering a patient : ${err}`);
-      const error = await handleErrors(err, req.body);
+      const error = await handleErrorsForPatient(err, req.body);
       if (error.patientFound) {
         res.status(200).json({
           message: 'patient already registered by this phone number',
@@ -42,6 +65,37 @@ module.exports = {
           error,
         });
       }
+    }
+  },
+  createReport: async function (req, res) {
+    try {
+      const patient = await Patient.findById(req.params.id);
+      if (!patient) {
+        /* if patient id provided is not found in db */
+        res.status(400).json({
+          message: 'the patient id provided not found',
+        });
+      } else {
+        /* create report */
+        let { status, doctorComments } = req.body;
+        doctorComments = doctorComments ? doctorComments : '';
+        const report = await Report.create({
+          byDoctor: res.locals.doctor.id,
+          patientId: req.params.id,
+          status,
+          doctorComments,
+        });
+        res.status(201).json({
+          message: 'report created',
+          report,
+        });
+      }
+    } catch (err) {
+      const error = handleErrorsForReport(err);
+      res.status(400).send({
+        message: 'error in creating a report',
+        error,
+      });
     }
   },
 };
--- routes/patientsRouter.js
+++ routes/patientsRouter.js
@@ -5,7 +5,11 @@ const patientsController = require('../controller/patientsController');
 const { requireDoctor } = require('../middleware/authMiddleware');
 
 router.post('/register', requireDoctor, patientsController.register);
-// router.post('/create_report/:id', patientsController.createReport);
+router.post(
+  '/:id/create_report',
+  requireDoctor,
+  patientsController.createReport
+);
 // router.get('/all_reports/:id', patientsController.getAllReports);
 // router.delete('/delete/:id', patientsController.delete);
 
