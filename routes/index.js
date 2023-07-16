640af4d4f56bf790e83e13054c7343c5a271c3ca
vibhavk03 <vibhavkashyap3@gmail.com>
2023-07-16 17:38:08 +0530

    removing redundant code

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,6 @@ const router = express.Router();
 const doctorsRouter = require('./doctorsRouter');
 const patientsRouter = require('./patientsRouter');
 const reportsRouter = require('./reportsRouter');
-const { checkUser } = require('../middleware/authMiddleware');
-
-router.get('*', checkUser);
-
-router.get('/health', function (req, res) {
-  res.json({ 'health-check': 'green' });
-});
 
 router.use('/doctors', doctorsRouter);
 // router.use('/patients', patientsRouter);
